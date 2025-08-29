640086a62e4917408bbcdebe581f4cd3537d2a78
Abdillah773 <abdillaramadhan020@gmail.com>
2025-08-28 22:38:00 -0700

    Change port to 5002

--- app.py
+++ app.py
@@ -201,4 +201,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
         create_default_user()
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5002)  # Badilisha port kuwa 5002
